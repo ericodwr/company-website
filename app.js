35f3a5febec4deff1e6c61146b13eafc1abdaa7e
ericodwr <ericodr@upnvj.ac.id>
2022-01-03 22:14:07 +0700

    update

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-const connectDB = require('./db/connect.js')
+const connectDB = require('./db/connect.js');
 const express = require('express');
-const app = express()
-const port = 8080
-const tasks = require('./routes/tasks.js')
-
+const app = express();
+const port = 8080;
+const tasks = require('./routes/tasks.js');
 
 // midddleware
 
-app.use(express.json())
-app.use(express.static('./public'))
+app.use(express.json());
+app.use(express.static('./public'));
 
 // routes
 
-app.use('/api/v1', tasks)
+app.use('/api/v1', tasks);
 
-const start =  async () => {
+const start = async () => {
   try {
-    await connectDB()
-    app.listen(port, console.log(`Server ${port} is running`))
+    await connectDB();
+    app.listen(port, console.log(`Server ${port} is running`));
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-start()
-
+start();
